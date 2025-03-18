f244dfc51e9a35c9092e3401ff73bd60ec936bb3
jasonleejs <jasonleejs12345@gmail.com>
2025-03-18 15:04:25 -0400

    test commit

---
+++ Unit 2: Classes and Objects/2.6_String_objects/charOperations.java
@@ -0,0 +1,29 @@
+public class CharOperations {
+    
+    public static void main(String args[]) {
+        System.out.println("Expected = A, Actual = " + toUpper('a'));
+        // Write your tests here, similar to above
+        
+    }
+    
+    // 1) 
+    // toUpper: char => char
+    // converts any lowercase alphabet char to its respective uppercase char, 
+    // otherwise returns the original char
+    // ====== Your Code Here ======
+
+
+    // ============================
+
+    
+    // 2) 
+    // toLower: char => char
+    // similar to toUpper, but converts any uppercase alphabet char to its respective lowercase char
+    // otherwise returns the original char
+    // ====== Your Code Here ======
+
+
+    // ============================
+
+    
+}
