63a9ffa18271ff7b2978709a7ba7f319c56976fa
jasonleejs <jasonleejs12345@gmail.com>
2025-03-30 16:08:44 -0400

    Mar 29 Lesson

--- Unit_2_Classes_and_Objects/2.7_String_methods/StringMethodsExercise.java
+++ Unit_2_Classes_and_Objects/2.7_String_methods/StringMethodsExercise.java
@@ -7,70 +7,75 @@ public class StringMethodsExercise {
         String str2 = new String("Hello");
         
         // Compare them using == and .equals()
-        System.out.println(str1 == str2);  // Expected: ???
-        System.out.println(str1.equals(str2));  // Expected: ???
+        // System.out.println(str1 == str2);  // Expected: false
+        // System.out.println(str1.equals(str2));  // Expected: true
 
-        // 2. String Length
-        // Predict the output of this code
-        String name = "Programming";
-        System.out.println(name.length());  // Expected: ???
+    //     // 2. String Length
+    //     // Predict the output of this code
+        // String name = "Programming";
+        // System.out.println(name.length());  // Expected: ???
 
-        // 3. Extracting Substrings
-        // What will be printed?
-        String word = "engineering";
-        System.out.println(word.substring(3, 7));  // Expected: ???
+    //     // 3. Extracting Substrings
+    //     // What will be printed?
+        // String word = "engineering";
+        // System.out.println(word.substring(3, 7));  // Expected: ???
 
-        // 4. Single Character Substring
-        // What will be printed?
-        String word2 = "example";
-        System.out.println(word2.substring(2, 3));  // Expected: ???
+    //     // 4. Single Character Substring
+    //     // What will be printed?
+        // String word2 = "example";
+        // System.out.println(word2.substring(2, 3));  // Expected: ???
 
-        // 5. Finding an Index
-        String phrase = "hello world";
-        System.out.println(phrase.indexOf("o"));  // Expected: ???
-        System.out.println(phrase.indexOf("z"));  // Expected: ???
+    //     // 5. Finding an Index
+        // String phrase = "hello world";
+        // System.out.println(phrase.indexOf("o"));  // Expected: 4
+        // System.out.println(phrase.indexOf("z"));  // When indexOf gets a letter that doesn't appear in the string, outputs -1
 
-        // 6. Checking String Equality
+    //     // 6. Checking String Equality
         String name1 = "Karel";
         String name2 = "Karel";
         String name3 = new String("Karel");
+        String name4 = new String("Karel");
 
-        System.out.println(name1.equals(name2));  // Expected: ???
-        System.out.println(name1.equals(name3));  // Expected: ???
-        System.out.println(name1 == name2);  // Expected: ???
-        System.out.println(name1 == name3);  // Expected: ???
+        // System.out.println(name1.equals(name2));  // Expected: true
+        // System.out.println(name1.equals(name3));  // Expected: true
+        // System.out.println(name1 == name2);  // Expected: true
+        // System.out.println(name1 == name3);  // Expected: false
+        // System.out.println(name3 == name4); // Expected: false
 
-        // 7. Comparing Strings
+    //     // 7. Comparing Strings
         String fruit = "Apple";
-        System.out.println(fruit.compareTo("Banana")); // Expected: ???
-        System.out.println(fruit.compareTo("Apple")); // Expected: ???
-        System.out.println(fruit.compareTo("Apricot")); // Expected: ???
+        System.out.println(fruit.compareTo("Banana")); // Expected: -1
 
-        // 8. Extract Initials (Fill in the method)
-        System.out.println(getInitials("John Doe"));  // Expected: "J.D."
+        System.out.println(fruit.compareTo("Apple")); // Expected: 0
+        // fruit.compareTo("Apple") => 0 => (fruit == "Apple")
 
-        // 9. Find the First and Last Character of a String
-        System.out.println(firstAndLast("Programming")); // Expected: "Pg"
+        System.out.println(fruit.compareTo("Apricot")); // Expected: -2
 
-        // 10. Count Occurrences of a Character
-        System.out.println(countOccurrences("banana", 'a')); // Expected: ???
-    }
+    //     // 8. Extract Initials (Fill in the method)
+    //     System.out.println(getInitials("John Doe"));  // Expected: "J.D."
 
-    // 8. Method to get initials from full name
-    public static String getInitials(String fullName) {
-        // TODO: Implement logic to extract initials
-        return "";
-    }
+    //     // 9. Find the First and Last Character of a String
+    //     System.out.println(firstAndLast("Programming")); // Expected: "Pg"
 
-    // 9. Method to return the first and last character of a string
-    public static String firstAndLast(String str) {
-        // TODO: Implement logic
-        return "";
-    }
+    //     // 10. Count Occurrences of a Character
+    //     System.out.println(countOccurrences("banana", 'a')); // Expected: ???
+    // }
 
-    // 10. Method to count occurrences of a character in a string
-    public static int countOccurrences(String str, char ch) {
-        // TODO: Implement logic
-        return 0;
+    // // 8. Method to get initials from full name
+    // public static String getInitials(String fullName) {
+    //     // TODO: Implement logic to extract initials
+    //     return "";
+    // }
+
+    // // 9. Method to return the first and last character of a string
+    // public static String firstAndLast(String str) {
+    //     // TODO: Implement logic
+    //     return "";
+    // }
+
+    // // 10. Method to count occurrences of a character in a string
+    // public static int countOccurrences(String str, char ch) {
+    //     // TODO: Implement logic
+    //     return 0;
     }
 }
